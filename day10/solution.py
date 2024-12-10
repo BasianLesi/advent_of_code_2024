c0966191f6162994e9dcae6a5f03eb7060baf0b2
Basian Lesi <lroni10094@gmail.com>
2024-12-10 22:50:08 +0100

    refactor

--- day10/solution.py
+++ day10/solution.py
@@ -16,8 +16,6 @@ def down_pos(pos):
 def check_in_bounds(pos, map):
     return 0 <= pos[0] < map.shape[0] and 0 <= pos[1] < map.shape[1]
 
-counter = 0
-
 def find_path(start, end, map):
     start = tuple(start)
     end = tuple(end)
@@ -43,8 +41,6 @@ def find_path(start, end, map):
     return sum
 
 
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename", type=str, default="sample", help="The filename to be parsed")  
 parser.add_argument("-d", "--debug", action="store_true", default=False, help="The filename to be parsed")  
@@ -56,15 +52,12 @@ sum1, sum2 = 0, 0
 with open(filename, "r") as f:
   df = f.read()
 
-dt = df
-rows = dt.strip().split('\n')
+rows = df.strip().split('\n')
 
 
 matrix = np.array([[int(x) for x in row] for row in rows])
 start = np.column_stack(np.where(matrix == 0))
 end = np.column_stack(np.where(matrix == 9))
-
-sum1 = 0
 
 for s in start:
     for e in end:
